38522011b3fcd515cd8d23ea362c11d133ac7113
yangle <mr.yang0229@foxmail.com>
2024-07-26 11:32:47 +0800

    解决ThreadPoolExecutor线程池，提交任务时，线程池已经关闭导致的RejectedExecutionException异常问题
    Process Name: 'com.ckl.forstudy.xzy:download'
    Thread Name: 'Thread-162'
    Back traces starts.
    java.util.concurrent.RejectedExecutionException: Task com.arialyy.aria.http.download.HttpDFileInfoTask@b51af96 rejected from java.util.concurrent.ThreadPoolExecutor@94ba717[Shutting down, pool size = 4, active threads = 4, queued tasks = 0, completed tasks = 0]
    	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2086)
    	at java.util.concurrent.ThreadPoolExecutor.reject(ThreadPoolExecutor.java:848)
    	at java.util.concurrent.ThreadPoolExecutor.execute(ThreadPoolExecutor.java:1394)
    	at com.arialyy.aria.http.download.HttpDGInfoTask$2.run(Unknown Source:117)
    	at java.lang.Thread.run(Thread.java:923)
    Back traces ends.

--- HttpComponent/src/main/java/com/arialyy/aria/http/download/HttpDGInfoTask.java
+++ HttpComponent/src/main/java/com/arialyy/aria/http/download/HttpDGInfoTask.java
@@ -27,6 +27,7 @@ import com.arialyy.aria.exception.AriaHTTPException;
 import com.arialyy.aria.http.HttpTaskOption;
 import com.arialyy.aria.util.ALog;
 import com.arialyy.aria.util.CommonUtil;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -35,187 +36,196 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 组合任务文件信息，用于获取长度未知时，组合任务的长度
  */
 public final class HttpDGInfoTask implements IInfoTask {
-  private String TAG = CommonUtil.getClassName(this);
-  private Callback callback;
-  private DGTaskWrapper wrapper;
-  private final Object LOCK = new Object();
-  private ExecutorService mPool = null;
-  private boolean getLenComplete = false;
-  private AtomicInteger count = new AtomicInteger();
-  private AtomicInteger failCount = new AtomicInteger();
-  private boolean isStop = false, isCancel = false;
+    private String TAG = CommonUtil.getClassName(this);
+    private Callback callback;
+    private DGTaskWrapper wrapper;
+    private final Object LOCK = new Object();
+    private ExecutorService mPool = null;
+    private boolean getLenComplete = false;
+    private AtomicInteger count = new AtomicInteger();
+    private AtomicInteger failCount = new AtomicInteger();
+    private boolean isStop = false, isCancel = false;
 
-  public interface DGInfoCallback extends Callback {
+    public interface DGInfoCallback extends Callback {
+
+        /**
+         * 子任务失败
+         */
+        void onSubFail(DownloadEntity subEntity, AriaHTTPException e, boolean needRetry);
+
+        /**
+         * 组合任务停止
+         */
+        void onStop(long len);
+    }
 
     /**
-     * 子任务失败
+     * 子任务回调
      */
-    void onSubFail(DownloadEntity subEntity, AriaHTTPException e, boolean needRetry);
-
-    /**
-     * 组合任务停止
-     */
-    void onStop(long len);
-  }
-
-  /**
-   * 子任务回调
-   */
-  private Callback subCallback = new Callback() {
-    @Override public void onSucceed(String url, CompleteInfo info) {
-      count.getAndIncrement();
-      checkGetSizeComplete(count.get(), failCount.get());
-      ALog.d(TAG, "获取子任务信息完成");
-    }
-
-    @Override public void onFail(AbsEntity entity, AriaException e, boolean needRetry) {
-      ALog.e(TAG, String.format("获取文件信息失败，url：%s", ((DownloadEntity) entity).getUrl()));
-      count.getAndIncrement();
-      failCount.getAndIncrement();
-      ((DGInfoCallback) callback).onSubFail((DownloadEntity) entity, new AriaHTTPException(
-          String.format("子任务获取文件长度失败，url：%s", ((DownloadEntity) entity).getUrl())), needRetry);
-      checkGetSizeComplete(count.get(), failCount.get());
-    }
-  };
-
-  HttpDGInfoTask(DGTaskWrapper wrapper) {
-    this.wrapper = wrapper;
-  }
-
-  /**
-   * 停止
-   */
-  @Override
-  public void stop() {
-    isStop = true;
-    if (mPool != null) {
-      mPool.shutdown();
-    }
-  }
-
-  @Override public void cancel() {
-    isCancel = true;
-    if (mPool != null) {
-      mPool.shutdown();
-    }
-  }
-
-  @Override public void run() {
-    // 如果是isUnknownSize()标志，并且获取大小没有完成，则直接回调onStop
-    if (mPool != null && !getLenComplete) {
-      ALog.d(TAG, "获取长度未完成的情况下，停止组合任务");
-      mPool.shutdown();
-      ((DGInfoCallback)callback).onStop(0);
-      return;
-    }
-    // 处理组合任务大小未知的情况
-    if (wrapper.isUnknownSize()) {
-      mPool = Executors.newCachedThreadPool();
-      getGroupSize();
-      try {
-        synchronized (LOCK) {
-          LOCK.wait();
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      if (!mPool.isShutdown()) {
-        mPool.shutdown();
-      }
-    } else {
-      for (DTaskWrapper wrapper : wrapper.getSubTaskWrapper()) {
-        cloneHeader(wrapper);
-      }
-      callback.onSucceed(wrapper.getKey(), new CompleteInfo());
-    }
-  }
-
-  /*
-   * 获取组合任务大小，使用该方式获取到的组合任务大小，子任务不需要再重新获取文件大小
-   */
-  private void getGroupSize() {
-    new Thread(new Runnable() {
-      @Override public void run() {
-        for (DTaskWrapper dTaskWrapper : wrapper.getSubTaskWrapper()) {
-          DownloadEntity subEntity = dTaskWrapper.getEntity();
-          if (subEntity.getFileSize() > 0) {
+    private Callback subCallback = new Callback() {
+        @Override
+        public void onSucceed(String url, CompleteInfo info) {
             count.getAndIncrement();
-            if (subEntity.getCurrentProgress() < subEntity.getFileSize()) {
-              // 如果没有完成需要拷贝一份数据
-              cloneHeader(dTaskWrapper);
-            }
             checkGetSizeComplete(count.get(), failCount.get());
-            continue;
-          }
-          cloneHeader(dTaskWrapper);
-          HttpDFileInfoTask infoTask = new HttpDFileInfoTask(dTaskWrapper);
-          infoTask.setCallback(subCallback);
-          mPool.execute(infoTask);
+            ALog.d(TAG, "获取子任务信息完成");
         }
-      }
-    }).start();
-  }
 
-  /**
-   * 检查组合任务大小是否获取完成，获取完成后取消阻塞，并设置组合任务大小
-   */
-  private void checkGetSizeComplete(int count, int failCount) {
-    if (isStop || isCancel) {
-      ALog.w(TAG, "任务已停止或已取消，isStop = " + isStop + ", isCancel = " + isCancel);
-      notifyLock();
-      return;
+        @Override
+        public void onFail(AbsEntity entity, AriaException e, boolean needRetry) {
+            ALog.e(TAG, String.format("获取文件信息失败，url：%s", ((DownloadEntity) entity).getUrl()));
+            count.getAndIncrement();
+            failCount.getAndIncrement();
+            ((DGInfoCallback) callback).onSubFail((DownloadEntity) entity, new AriaHTTPException(
+                    String.format("子任务获取文件长度失败，url：%s", ((DownloadEntity) entity).getUrl())), needRetry);
+            checkGetSizeComplete(count.get(), failCount.get());
+        }
+    };
+
+    HttpDGInfoTask(DGTaskWrapper wrapper) {
+        this.wrapper = wrapper;
     }
-    if (failCount == wrapper.getSubTaskWrapper().size()) {
-      callback.onFail(wrapper.getEntity(), new AriaHTTPException("获取子任务长度失败"), false);
-      notifyLock();
-      return;
+
+    /**
+     * 停止
+     */
+    @Override
+    public void stop() {
+        isStop = true;
+        if (mPool != null) {
+            mPool.shutdown();
+        }
     }
-    if (count == wrapper.getSubTaskWrapper().size()) {
-      long size = 0;
-      for (DTaskWrapper wrapper : wrapper.getSubTaskWrapper()) {
-        size += wrapper.getEntity().getFileSize();
-      }
-      wrapper.getEntity().setConvertFileSize(CommonUtil.formatFileSize(size));
-      wrapper.getEntity().setFileSize(size);
-      wrapper.getEntity().update();
-      getLenComplete = true;
-      ALog.d(TAG, String.format("获取组合任务长度完成，组合任务总长度：%s，失败的子任务数：%s", size, failCount));
-      callback.onSucceed(wrapper.getKey(), new CompleteInfo());
-      notifyLock();
+
+    @Override
+    public void cancel() {
+        isCancel = true;
+        if (mPool != null) {
+            mPool.shutdown();
+        }
     }
-  }
 
-  private void notifyLock() {
-    synchronized (LOCK) {
-      LOCK.notifyAll();
+    @Override
+    public void run() {
+        // 如果是isUnknownSize()标志，并且获取大小没有完成，则直接回调onStop
+        if (mPool != null && !getLenComplete) {
+            ALog.d(TAG, "获取长度未完成的情况下，停止组合任务");
+            if (!mPool.isShutdown()) {
+                mPool.shutdown();
+            }
+            ((DGInfoCallback) callback).onStop(0);
+            return;
+        }
+        // 处理组合任务大小未知的情况
+        if (wrapper.isUnknownSize()) {
+            mPool = Executors.newCachedThreadPool();
+            getGroupSize();
+            try {
+                synchronized (LOCK) {
+                    LOCK.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (!mPool.isShutdown()) {
+                mPool.shutdown();
+            }
+        } else {
+            for (DTaskWrapper wrapper : wrapper.getSubTaskWrapper()) {
+                cloneHeader(wrapper);
+            }
+            callback.onSucceed(wrapper.getKey(), new CompleteInfo());
+        }
     }
-  }
 
-  /**
-   * 子任务使用父包裹器的属性
-   */
-  private void cloneHeader(DTaskWrapper taskWrapper) {
-    HttpTaskOption groupOption = (HttpTaskOption) wrapper.getTaskOption();
-    HttpTaskOption subOption = new HttpTaskOption();
+    /*
+     * 获取组合任务大小，使用该方式获取到的组合任务大小，子任务不需要再重新获取文件大小
+     */
+    private void getGroupSize() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (DTaskWrapper dTaskWrapper : wrapper.getSubTaskWrapper()) {
+                    DownloadEntity subEntity = dTaskWrapper.getEntity();
+                    if (subEntity.getFileSize() > 0) {
+                        count.getAndIncrement();
+                        if (subEntity.getCurrentProgress() < subEntity.getFileSize()) {
+                            // 如果没有完成需要拷贝一份数据
+                            cloneHeader(dTaskWrapper);
+                        }
+                        checkGetSizeComplete(count.get(), failCount.get());
+                        continue;
+                    }
+                    cloneHeader(dTaskWrapper);
+                    HttpDFileInfoTask infoTask = new HttpDFileInfoTask(dTaskWrapper);
+                    infoTask.setCallback(subCallback);
+                    mPool.execute(infoTask);
+                }
+            }
+        }).start();
+    }
 
-    // 设置属性
-    subOption.setFileLenAdapter(groupOption.getFileLenAdapter());
-    subOption.setFileNameAdapter(groupOption.getFileNameAdapter());
-    subOption.setUseServerFileName(groupOption.isUseServerFileName());
+    /**
+     * 检查组合任务大小是否获取完成，获取完成后取消阻塞，并设置组合任务大小
+     */
+    private void checkGetSizeComplete(int count, int failCount) {
+        if (isStop || isCancel) {
+            ALog.w(TAG, "任务已停止或已取消，isStop = " + isStop + ", isCancel = " + isCancel);
+            notifyLock();
+            return;
+        }
+        if (failCount == wrapper.getSubTaskWrapper().size()) {
+            callback.onFail(wrapper.getEntity(), new AriaHTTPException("获取子任务长度失败"), false);
+            notifyLock();
+            return;
+        }
+        if (count == wrapper.getSubTaskWrapper().size()) {
+            long size = 0;
+            for (DTaskWrapper wrapper : wrapper.getSubTaskWrapper()) {
+                size += wrapper.getEntity().getFileSize();
+            }
+            wrapper.getEntity().setConvertFileSize(CommonUtil.formatFileSize(size));
+            wrapper.getEntity().setFileSize(size);
+            wrapper.getEntity().update();
+            getLenComplete = true;
+            ALog.d(TAG, String.format("获取组合任务长度完成，组合任务总长度：%s，失败的子任务数：%s", size, failCount));
+            callback.onSucceed(wrapper.getKey(), new CompleteInfo());
+            notifyLock();
+        }
+    }
 
-    subOption.setFileNameAdapter(groupOption.getFileNameAdapter());
-    subOption.setRequestEnum(groupOption.getRequestEnum());
-    subOption.setHeaders(groupOption.getHeaders());
-    subOption.setProxy(groupOption.getProxy());
-    subOption.setParams(groupOption.getParams());
-    taskWrapper.setTaskOption(subOption);
-  }
+    private void notifyLock() {
+        synchronized (LOCK) {
+            LOCK.notifyAll();
+        }
+    }
 
-  @Override public void setCallback(Callback callback) {
-    this.callback = callback;
-  }
+    /**
+     * 子任务使用父包裹器的属性
+     */
+    private void cloneHeader(DTaskWrapper taskWrapper) {
+        HttpTaskOption groupOption = (HttpTaskOption) wrapper.getTaskOption();
+        HttpTaskOption subOption = new HttpTaskOption();
 
-  @Override public void accept(ILoaderVisitor visitor) {
-    visitor.addComponent(this);
-  }
+        // 设置属性
+        subOption.setFileLenAdapter(groupOption.getFileLenAdapter());
+        subOption.setFileNameAdapter(groupOption.getFileNameAdapter());
+        subOption.setUseServerFileName(groupOption.isUseServerFileName());
+
+        subOption.setFileNameAdapter(groupOption.getFileNameAdapter());
+        subOption.setRequestEnum(groupOption.getRequestEnum());
+        subOption.setHeaders(groupOption.getHeaders());
+        subOption.setProxy(groupOption.getProxy());
+        subOption.setParams(groupOption.getParams());
+        taskWrapper.setTaskOption(subOption);
+    }
+
+    @Override
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public void accept(ILoaderVisitor visitor) {
+        visitor.addComponent(this);
+    }
 }
